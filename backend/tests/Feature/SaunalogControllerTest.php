050c3cf558d354de9b2c34173bc40a3b5fe40c8f
mattu23 <hirokiii_27@yahoo.co.jp>
2024-02-06 01:14:09 +0900

    サウナログ作成のテストコード追加

--- backend/tests/Feature/SaunalogControllerTest.php
+++ backend/tests/Feature/SaunalogControllerTest.php
@@ -5,18 +5,32 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+
 
 class SaunalogControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
+    //サウナログの新規作成テスト
+    /** @test */
+    public function testCreateSaunalog() 
     {
-        $response = $this->get('/');
+        //テスト用のユーザーデータをデータベースに実際に挿入
+        $user = User::factory()->create();
+        //テスト中に指定したユーザーとして認証された状態を模擬
+        $this->actingAs($user, 'api');
 
-        $response->assertStatus(200);
+        $logData = [
+            'name' => 'サウナ東京',
+            'area' => '東京都 港区',
+            'rank' => 5,
+            'comment' => '最高のサウナ体験でした！',
+        ];
+
+        //指定されたルートに対してJSON形式のPOSTリクエストを送信。APIエンドポイントに対するHTTPリクエストをテスト
+        $response = $this->postJson('/api/saunalog', $logData);
+        //レスポンスのステータスコードが201（Created）であることを検証
+        $response->assertStatus(201);
+        //指定したデータがデータベースのsaunalogsテーブルに存在することを検証
+        $this->assertDatabaseHas('saunalogs', $logData);
     }
-
-    
 }
