d76886719239ec60a2050f9192851b54284700c5
mattu23 <hirokiii_27@yahoo.co.jp>
2024-02-10 18:37:18 +0900

    refactor：SaunalogServiceの各メソッドを、より具体的なエラーを返すよう修正

--- backend/app/Services/SaunalogService.php
+++ backend/app/Services/SaunalogService.php
@@ -5,8 +5,8 @@ namespace App\Services;
 use App\Models\Saunalog;
 use App\Models\User;
 use Illuminate\Database\Eloquent\Collection;
-use App\Exceptions\CustomException;
 use App\Exceptions\NotFoundException;
+use App\Exceptions\SystemException;
 
 
 
@@ -17,69 +17,59 @@ class SaunalogService
     //ユーザーに紐づくサウナログ一覧の表示
     public function getLogsByUser($userId): Collection 
     {
-      try {
-          return Saunalog::where('userId', $userId)->get();
-      } catch(\Exception $e) {
-          throw new CustomException('サウナログの一覧表示に失敗しました。');
-      }
+        $logs = Saunalog::where('userId', $userId)->get();
+        //「ログが見つからない＝Whereメソッドが空」なので、そのチェックをし空の場合は例外を投げる
+        if($logs->isEmpty()) {
+          throw new NotFoundException('ユーザーに紐づくサウナログが見つかりません。');
+        }
+        return $logs;
     }
 
 
     //特定のサウナログの表示
     public function getLogById($id): ?Saunalog
     {
-      try {
-          $saunalog = Saunalog::find($id);
-          if(!$saunalog) {
-            throw new NotFoundException('特定のサウナログが見つかりません。');
-          }
-          return $saunalog;
-      } catch(\Exception $e) {
-          throw new CustomException('サウナログの表示に失敗しました。');
-      }
+        $saunalog = Saunalog::find($id);
+        //findメソッドに対し例外は発生しないため、NotFoundのみで表記
+        if(!$saunalog) {
+          throw new NotFoundException('特定のサウナログが見つかりません。');
+        }
+        return $saunalog;
     }
 
     //サウナログの新規作成
     public function createLog(array $data, User $user): Saunalog
     {
-      try {
-          $saunalog = new Saunalog($data);
-          $saunalog->user()->associate($user);
-          $saunalog->save();
+        $saunalog = new Saunalog($data);
+        $saunalog->user()->associate($user);
+        if($saunalog->save()) {
           return $saunalog;
-      } catch(\Exception $e) {
-          throw new CustomException('サウナログの新規作成に失敗しました。');
-      }
+        } else {
+          throw new SystemException('ログの新規作成に失敗しました。');
+        }
     }
 
     //サウナログの編集
     public function updateLogById($id, array $data): Saunalog
     {
-      try {
-          $saunalog = Saunalog::find($id);
-          if(!$saunalog) {
-            throw new NotFoundException('編集対象のサウナログが見つかりません。');
-          }
-          $saunalog->update($data);
-          return $saunalog;
-      } catch(\Exception $e) {
-          throw new CustomException('サウナログの編集に失敗しました。');
-      }
+        $saunalog = Saunalog::find($id);
+        //findメソッドに対し例外は発生しないため、NotFoundのみで表記
+        if(!$saunalog) {
+          throw new NotFoundException('編集対象のサウナログが見つかりません。');
+        }
+        $saunalog->update($data);
+        return $saunalog;
     }
 
     //サウナログの削除
     public function deleteLogById($id): bool
     {
-      try {
-          $saunalog = Saunalog::find($id);
-          if (!$saunalog) {
-            throw new NotFoundException('削除対象のサウナログが見つかりません。');
-         }
-          return $saunalog->delete();
-      } catch(\Exception $e) {
-        throw new CustomException('サウナログの削除に失敗しました。');
-      } 
+        $saunalog = Saunalog::find($id);
+        //findメソッドに対し例外は発生しないため、NotFoundのみで表記
+        if (!$saunalog) {
+          throw new NotFoundException('削除対象のサウナログが見つかりません。');
+        }
+        return $saunalog->delete();
     }
-
 
 }
