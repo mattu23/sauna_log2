1d27a703832d1d7c1c793bfde0ade99c40b59c24
mattu23 <hirokiii_27@yahoo.co.jp>
2024-02-12 23:48:01 +0900

    add：ユーザーの新規登録メソッドのテスト追加

--- backend/tests/Feature/UserControllerTest.php
+++ backend/tests/Feature/UserControllerTest.php
@@ -5,16 +5,31 @@ namespace Tests\Feature;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
+use App\Models\User;
+use App\Models\Saunalog;
 
-class UserApiTest extends TestCase
+
+class UserControllerTest extends TestCase
 {
-    /**
-     * A basic feature test example.
-     */
-    public function test_example(): void
-    {
-        $response = $this->get('/');
+    use RefreshDatabase;
 
-        $response->assertStatus(200);
+    /** @test */
+    //ユーザー新規作成の正常テスト
+    public function testSignUpSuccessfully()
+    {
+        //バリデーションルールに適合するテスト用のユーザーデータを準備
+        $testUser = [
+            'username' => 'TestUser1',
+            'email' => 'test@example.com',
+            'password' => 'password1234',
+        ];
+        //新規登録APIを呼び出し
+        $response = $this->postJson('/api/signup', $testUser);
+        //レスポンスステータスコードが201であることを検証
+        $response->assertStatus(201);
+        //データベースにユーザーが正しく保存されていることを検証
+        $this->assertDatabaseHas('users', [
+            'email' => 'test@example.com'
+        ]);
     }
 }
