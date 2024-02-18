6308654582de6c84c25fcdeafd741ce5771237cf
mattu23 <hirokiii_27@yahoo.co.jp>
2024-02-18 15:40:02 +0900

    revert：一時的にローカル環境で実装する為の接続設定の修正

--- frontend/.nuxt/utils.js
+++ frontend/.nuxt/utils.js
@@ -192,14 +192,14 @@ export async function setContext (app, context) {
   if (!app.context) {
     app.context = {
       isStatic: process.static,
-      isDev: false,
+      isDev: true,
       isHMR: false,
       app,
       store: app.store,
       payload: context.payload,
       error: context.error,
       base: app.router.options.base,
-      env: {"API_ENDPOINT":"http://52.198.90.29/api","API_BASE_URL":"http://52.198.90.29/api"}
+      env: {"API_ENDPOINT":"http://localhost:8000/api","API_BASE_URL":"http://localhost:8000/api"}
     }
     // Only set once
 
@@ -283,7 +283,7 @@ export async function setContext (app, context) {
   app.context.next = context.next
   app.context._redirected = false
   app.context._errored = false
-  app.context.isHMR = false
+  app.context.isHMR = Boolean(context.isHMR)
   app.context.params = app.context.route.params || {}
   app.context.query = app.context.route.query || {}
 }
@@ -301,6 +301,9 @@ export function middlewareSeries (promises, appContext, renderState) {
 export function promisify (fn, context) {
   let promise
   if (fn.length === 2) {
+      console.warn('Callback-based asyncData, fetch or middleware calls are deprecated. ' +
+        'Please switch to promises or async/await syntax')
+
     // fn(context, callback)
     promise = new Promise((resolve) => {
       fn(context, function (err, data) {
